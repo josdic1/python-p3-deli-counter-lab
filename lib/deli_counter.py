cb47bca941530fe2c7cbb713fa4c6fc93d81c095
josdic1 <joshuadicker@gmail.com>
2025-05-14 15:06:41 -0400

    deli done

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -1,8 +1,10 @@
-people = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+KATZ_DELI = []
+OTHER_DELI = ["Logan", "Avi", "Spencer"]
+ANOTHER_DELI = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
-katz_deli = ["Ruchi", "Jason"]
 line_text = "The line is currently: "
-
+next_in_line_alert = "Currently serving "
+empty_line_alert = "There is nobody waiting to be served!"
 
 def line(line_names):
   if len(line_names) == 0:
@@ -14,16 +16,27 @@ def line(line_names):
       updated_line.append(f"{index + 1}. {name}")
     updated_str = " ".join(updated_line)
     output = f"{line_text}{updated_str}"
-    line_info = output
-    print(line_info)
+    print(output)
       
+
 def take_a_number(line, name):
-  add_name = line.append(name)
-  print(add_name)
+  formatted = []
+  line.append(name)
+  for index, l in enumerate(line):
+    p = f"{index}. {l}"
+    formatted.append(p)
+    position = len(formatted) 
+  print(f"Welcome, {name}. You are number {position} in line.")
 
-# f"Welcome, {name}. You are number {index} in line."
-print(take_a_number(katz_deli, "Paul"))
 
-def now_serving():
-    pass
+def now_serving(line):
+  if len(line) == 0:
+    print(empty_line_alert)
+  else:
+    current = line.pop(0)
+    print(f"{next_in_line_alert}{current}.")
+
+
+print(now_serving(OTHER_DELI)) 
+
  
